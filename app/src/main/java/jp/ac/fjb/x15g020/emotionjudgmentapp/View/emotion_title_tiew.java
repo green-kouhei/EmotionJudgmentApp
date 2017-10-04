bff7bbb24b79f440662a67b26db936cfe8a0072e
green-kouhei <kouhei_shoji@green-minto.com>
2017-10-04 11:25:01 +0900

    タイトル作ってる

--- app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp/View/emotion_title_tiew.java
+++ app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp/View/emotion_title_tiew.java
@@ -1,9 +1,17 @@
 package jp.ac.fjb.x15g020.emotionjudgmentapp.View;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Kouhei on 2017/10/02.
  */
 
-public class emotion_title_tiew {
 
+public class emotion_title_tiew extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
