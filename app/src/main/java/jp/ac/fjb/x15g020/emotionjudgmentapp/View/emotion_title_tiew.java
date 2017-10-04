076e1def714e8584f9d081553c46e31dc713e0af
green-kouhei <kouhei_shoji@green-minto.com>
2017-10-04 12:43:26 +0900

    変なエラー出てる。。。。

--- app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp/View/emotion_title_tiew.java
+++ app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp/View/emotion_title_tiew.java
@@ -2,12 +2,23 @@ package jp.ac.fjb.x15g020.emotionjudgmentapp.View;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Kouhei on 2017/10/02.
  */
 
 
+public class emotion_title_tiew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.emotion_title_1);
+    }
+}
+
+/*
 public class emotion_title_tiew extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,3 +26,4 @@ public class emotion_title_tiew extends Activity {
 
     }
 }
+ */
